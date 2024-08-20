0f0cd24b7ca029a72947fb87a54590f10b96ae4d
AydanRahimova <aydanragimova28@gmail.com>
2024-08-20 11:34:49 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static Connection connection;
+
+    static {
+        String url = "jdbc:postgresql://localhost:5432/matrix161";
+        String user = "postgres";
+        String password = "2810";
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS customer" +
+                    "(id SERIAL PRIMARY KEY,name VARCHAR(30),surname VARCHAR(30),birthday DATE,position VARCHAR(30))";
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Please enter an operation:" +
+                    "\n1-for viewing customers" +
+                    "\n2-for adding customer" +
+                    "\n3-for updating new customer" +
+                    "\n4-for removing a customer" +
+                    "\n5-for exit");
+            int choice = scanner.nextInt();
+            Operation.menu(choice);
+        }
+    }
+}
---
+++ src/Operation.java
@@ -0,0 +1,121 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Operation {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void menu(int choice) {
+        switch (choice) {
+            case 1:
+                viewCustomers(Main.connection);
+                break;
+            case 2:
+                addCustomer(Main.connection);
+                break;
+            case 3:
+                updateCustomer(Main.connection);
+                break;
+            case 4:
+                removeCustomer(Main.connection);
+                break;
+            case 5:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Please,enter a correct choice");
+        }
+    }
+
+    private static ResultSet viewCustomers(Connection connection) {
+        String sql = "SELECT * FROM customer";
+        Statement statement;
+        ResultSet resultSet;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                System.out.println(id + ".Customer:");
+                System.out.println("name:" + resultSet.getString("name"));
+                System.out.println("surname:" + resultSet.getString("surname"));
+                System.out.println("birthday:" + resultSet.getDate("birthday"));
+                System.out.println("position:" + resultSet.getString("position"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return resultSet;
+    }
+
+    private static int addCustomer(Connection connection) {
+        String sql = "INSERT INTO customer (name,surname,birthday,position)values(?,?,?,?)";
+        fillCustomer(connection, sql);
+        return 1;
+    }
+
+    private static int updateCustomer(Connection connection) {
+        System.out.println("Please,enter an id of customer who information you want to update:");
+        int id = scanner.nextInt();
+        if (checkForExistence(connection, id)) {
+            String sql = "UPDATE customer SET name = ?,surname = ?,birthday = ?,position= ? where id =" + id;
+            fillCustomer(connection, sql);
+            return 1;
+        }
+        return 0;
+    }
+
+    private static int removeCustomer(Connection connection) {
+        System.out.println("Please,enter an id of customer whose information you want to delete:");
+        int id = scanner.nextInt();
+        if (checkForExistence(connection, id)) {
+            String sql = "DELETE FROM customer where id =" + id;
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sql);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return 1;
+        }
+        return 0;
+    }
+
+
+    private static int fillCustomer(Connection connection, String sql) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            System.out.println("Enter a name:");
+            preparedStatement.setString(1, scanner.next());
+            System.out.println("Enter a surname");
+            preparedStatement.setString(2, scanner.next());
+            System.out.print("Enter birthday (yyyy-MM-dd): ");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate localDate = LocalDate.parse(scanner.next(), formatter);
+            Date sqlDate = Date.valueOf(localDate);
+            preparedStatement.setDate(3, sqlDate);
+            System.out.println("Enter a position:");
+            scanner.nextLine();
+            preparedStatement.setString(4, scanner.nextLine());//nextLine() islemir??
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 1;
+    }
+
+    private static boolean checkForExistence(Connection connection, int id) {
+        String checkSql = "SELECT id FROM customer where id=" + id;
+        try (Statement statement = connection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(checkSql)) {
+                if (!resultSet.next()) {
+                    System.out.println("Customer with " + id + " id does not exist");
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+
+}
